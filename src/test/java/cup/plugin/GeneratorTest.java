ce7adb0c7728bbc89f3651a62e2c23f697d699e3
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-04-04 11:59:20 +0200

    [Fix]: Improve  test case about Generator in order to not depend by local paths

--- src/test/java/cup/plugin/GeneratorTest.java
+++ src/test/java/cup/plugin/GeneratorTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -16,15 +15,17 @@ public class GeneratorTest {
     public void cupGenerate_Success() throws InterruptedException, MoreFileException, IOException {
         CommandAdapter commandAdapter = mock(CommandAdapter.class);
         CupFile cupFile = mock(CupFile.class);
+        String dir = System.getProperty("user.dir");
 
-        when(cupFile.retrieve("/Users/nicola/Nicola/projects/github/cup-maven-plugin/cupPath")).thenReturn("cupFilename");
+        when(cupFile.retrieve(dir + "/" + "cupPath")).thenReturn("cupFilename");
 
         Generator generator = new Generator(cupFile, commandAdapter);
         generator.cupGenerate("cupPath");
 
         ArgumentCaptor<String> command = ArgumentCaptor.forClass(String.class);
         verify(commandAdapter, times(1)).exec(command.capture());
-        List<String> capturedPeople = command.getAllValues();
-        assertEquals("java -jar /Users/nicola/.m2/repository/edu/princeton/cup/java-cup/10k/cup-0.10k.jar cupFilename", capturedPeople.get(0));
+        String result = command.getAllValues().get(0);
+        String expected = "java -jar " + System.getenv("HOME") + "/.m2/repository/edu/princeton/cup/java-cup/10k/cup-0.10k.jar cupFilename";
+        assertEquals(expected, result);
     }
 }
